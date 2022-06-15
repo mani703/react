24e16453334ce3c566a04aafd1a408c8162590bf
mani703 <mani703@naver.com>
2022-06-15 17:50:51 +0900

    react class component

--- react02/src/index.js
+++ react02/src/index.js
@@ -3,12 +3,44 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Car from './ex01';
+
+
+
+function Car3() {
+  return <h2>Car3!</h2>
+}
+
+class Car4 extends React.Component{
+  
+  constructor() {
+    super();
+    this.state = {msg: '클릭안됨'};
+  }
+
+  componentDidMount(){
+    console.log('componentDidMount');
+  }
+
+  componentDidUpdate(){
+    console.log('componentDidUpdate');
+  }
+
+  render(){
+    return(
+      <div>
+        <button onClick={()=>{ this.state={msg:'클릭됨'} }}>클릭</button>
+        <p>{this.state.msg}</p>
+      </div>
+    )
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+  <Car4/>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
