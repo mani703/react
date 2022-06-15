dbfab2f601c3fb13d003006f67998362952499f7
mani703 <mani703@naver.com>
2022-06-15 17:59:01 +0900

    react class component

--- react02/src/index.js
+++ react02/src/index.js
@@ -3,45 +3,59 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Car from './ex01';
+import Car from './Ex01';
 
+// const myElement = React.createElement('h1', {}, 'I do not use JSX!');
+// const myElement = <h1>React is {5 + 5} times better with JSX</h1>;
 
-
-function Car3() {
-  return <h2>Car3!</h2>
+class Car2 extends React.Component {
+  constructor(props){
+    super();
+    this.name=props.name;
+  }
+  render() {
+    return <h2>{this.name}!</h2>;
+  }
 }
 
+function Car3(props) {
+  return (
+    <>
+    <Car2 name={props.name}/>
+    <h2>Car3!</h2>
+    </>
+  );
+}
 class Car4 extends React.Component{
-  
-  constructor() {
-    super();
-    this.state = {msg: '클릭안됨'};
-  }
 
+  constructor(props){
+    super(props);
+    this.msg2='클릭안됨';
+    this.state = {msg:Number(props.num)};
+  }
   componentDidMount(){
-    console.log('componentDidMount');
+    console.log('componentDidMount()');
   }
-
   componentDidUpdate(){
-    console.log('componentDidUpdate');
+    console.log('componentDidUpdate()');
   }
-
   render(){
-    return(
+    return (
       <div>
-        <button onClick={()=>{ this.state={msg:'클릭됨'} }}>클릭</button>
+        <button onClick={()=>{this.setState({msg:this.state.msg+1});console.log(this.state.msg);}}>클릭</button>
         <p>{this.state.msg}</p>
       </div>
-    )
+    );
   }
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
-  <Car4/>
-  </>
+<>
+<Car4 num="100"/>
+</>
 );
+// ReactDOM.render(<p>Hello</p>, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
