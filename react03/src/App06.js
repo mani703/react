ad40d267fde26fcaea8409bddd8f8538cce91c45
mani703 <mani703@naver.com>
2022-06-16 15:08:21 +0900

    useEffect

---
+++ react03/src/App06.js
@@ -0,0 +1,29 @@
+
+import { useEffect, useState } from 'react';
+
+export default function App06(props) {
+    console.log('loading....');
+    const [msg, setMsg] = useState(1);
+    const [msg2, setMsg2] = useState("second");
+    
+    useEffect(function(){
+        console.log('useEffect....');
+    }, [msg, msg2]);
+
+    const btnHandler = () => {
+        setMsg(2);
+    };
+
+    const btnHandler2 = () => {
+        setMsg2('이벤트 발생');
+    };
+
+    return (
+        <div>
+            <h1>{msg} - {msg2}</h1>
+            <button onClick={btnHandler}>버튼</button>
+            <button onClick={btnHandler2}>버튼</button>
+            {console.log('after evnet...')}
+        </div>
+    );
+}
