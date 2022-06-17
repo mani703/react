9112f5b09cfa90741b86b6cd2f90f7b9df577e7c
mani703 <mani703@naver.com>
2022-06-17 17:51:29 +0900

    App.js

---
+++ react04/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Intro from './pages/Intro';
+import DeptList from './pages/DeptList';
+import Login from './pages/Login';
+import Mnav from './modules/Mnav';
+import DeptAdd from './pages/DeptAdd';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Mnav/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/intro" element={<Intro />} />
+        <Route path="/dept" element={<DeptList />} />
+        <Route path="/add" element={<DeptAdd />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
