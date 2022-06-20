56fd789c2ec7b7a725a6c27f44463e182d148562
mani703 <mani703@naver.com>
2022-06-20 12:53:17 +0900

    react project init

--- react05/src/App.js
+++ react05/src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Intro from './pages/Intro';
+import DeptList from './pages/DeptList';
+import DeptOne from './pages/DeptOne';
+import DeptAdd from './pages/DeptAdd';
+import LoginPage from './pages/LoginPage';
+import Join from './pages/Join';
+import Menubar from './modules/Menubar';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+      <Menubar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/intro" element={<Intro/>}/>
+        <Route path="/dept" element={<DeptList/>}/>
+        <Route path="/dept/:deptno" element={<DeptOne/>}/>
+        <Route path="/dept/add" element={<DeptAdd/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/join" element={<Join/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
